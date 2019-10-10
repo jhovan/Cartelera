170b7131d144608e2c9869032c9b6eb08aeeeb07
jhovan <jhovan.gallardo@gmail.com>
2019-10-10 16:34:08 -0500

    Initial Commit

---
+++ Cartelera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cartelera
+//
+//  Created by Jhovan Gallardo on 10/10/19.
+//  Copyright © 2019 UNAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
